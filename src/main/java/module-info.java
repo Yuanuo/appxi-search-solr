b354161af78daf716ca471b52c3eec9db53be946
Yuanuox <yuanuox@outlook.com>
2022-03-18 10:32:31 +0800

    调整依赖项机制并更新版本号

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,25 @@
 module appxi.search.solr {
-    requires static appxi.search.solr.aio;
-    requires static spring.data.solr;
-    requires static spring.data.commons;
+    requires transitive static appxi.search.solr.aio;
+
+    requires transitive static javax.servlet.api;
+    requires transitive static spring.core;
+    requires transitive static spring.context;
+    requires transitive static spring.beans;
+    requires transitive static spring.data.commons;
+    requires transitive static spring.data.solr;
+    requires transitive static spring.tx;
+    requires transitive com.fasterxml.jackson.annotation;
+    requires transitive com.fasterxml.jackson.core;
+    requires transitive com.fasterxml.jackson.databind;
+    requires transitive java.management;
+    requires transitive java.naming;
+
+    requires transitive org.slf4j;
+    requires transitive org.slf4j.simple;
+    requires transitive org.apache.commons.logging;
+    requires transitive org.apache.logging.log4j;
+    requires transitive static org.apache.logging.log4j.core;
+    requires transitive static org.apache.logging.log4j.slf4j;
 
     exports org.appxi.search.solr;
 
